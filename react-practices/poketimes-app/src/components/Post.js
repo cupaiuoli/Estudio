5384a8ea85a6e44fbdb027845db7dad2e3ffa837
= <lautaro.cupaiuoli@gmail.com>
2018-09-17 01:37:46 -0300

    React & Redux Practice

--- react-practices/poketimes-app/src/components/Post.js
+++ react-practices/poketimes-app/src/components/Post.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions'
+//import axios from 'axios';
 
 class Post extends Component {
-    state = {
+/*    state = {
         post: null
     }
     componentDidMount(props) {
@@ -16,13 +18,20 @@ class Post extends Component {
                 })
             })
     }
-
+*/
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
+    }
     render() {
-        const post = this.state.post 
+        //const post = this.state.post 
+        console.log(this.props);
+        const post = this.props.post 
             ? (
                 <div className="post">
-                    <h4>ID: {this.state.post.id} - {this.state.post.title}</h4>
-                    <p>{this.state.post.body}</p>
+                    <h4>ID: {this.props.post.id} - {this.props.post.title}</h4>
+                    <p>{this.props.post.body}</p>
+                    <button className="btn grey" onClick={this.handleClick}>DELETE POST</button>
                 </div>
             )
             : (
@@ -35,5 +44,17 @@ class Post extends Component {
         )
     }
 }
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    console.log(id);
+    return {
+        post: state.posts.find(post => post.id == id)
+    }
+}
 
-export default Post
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch(deletePost(id)) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
